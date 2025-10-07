14bd07065ef8b7cadedeb37da640e61b892d1da4
Who <w.who.am@proton.me>
2025-10-08 02:24:49 +0300

    prettier & eslint & global format

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  singleQuote: false,
+  semi: true,
+};
